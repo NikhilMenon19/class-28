99b52a3df013a0169043038f3a9cf2410bf7d96b
NikhilMenon19 <77450564+NikhilMenon19@users.noreply.github.com>
2021-02-22 20:40:51 +0530

    Add files via upload

---
+++ slingshot.js
@@ -0,0 +1,29 @@
+class SlingShot{
+constructor(bodyA,pointB){
+    var options={
+        bodyA:bodyA,
+        pointB:pointB,
+        stiffness:0.04,
+        length:10
+
+    }
+
+    this.pointB=pointB;
+
+    this.sling=Constraint.create(options)
+    World.add(world,this.sling);
+}
+
+ disconnect(){
+     this.sling.bodyA=null;
+ }
+
+display(){
+
+if(this.sling.bodyA){
+
+strokeWeight(4)
+line(this.sling.bodyA.position.x,this.sling.bodyA.position.y,this.pointB.x,this.pointB.y)
+}
+}
+}
